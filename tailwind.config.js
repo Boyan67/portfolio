d2d09a429c2af98c1647a63689770fe2c900842b
Boyan67 <118831149+Boyan-nolimit@users.noreply.github.com>
2023-03-01 12:07:38 +0200

    change color to blue

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,8 @@ module.exports = {
       colors: {
         main: {
           100: '#2455d2',
-          80: 'rgba(239,58,56,0.8)',
-          45: 'rgba(239,58,56,0.45)'
+          80: 'rgba(56,169,239,0.8)',
+          45: 'rgba(0,136,194,0.5)'
         },
         customGray: '#B9B9B9',
         customDarkGray: '#8C8C8C',
